d0b7f03679b1d74487fb51c21b9f20adc5f7164e
RecluseJean <jean.guevara.castro@gmail.com>
2021-09-24 16:03:40 -0500

    v.0.2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { InicioComponent } from './inicio/inicio.component';
+import { LoginComponent } from './login/login.component';
 
-const routes: Routes = [];
+
+
+const routes: Routes = [
+
+  { path: 'inicio', component: InicioComponent, data: { title: 'Perfil – Planillas' } },
+  { path: 'login', component: LoginComponent, data: { title: 'Signin – Planillas' } },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,34 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
-import { AdministracionComponent } from './modulos/administracion/administracion.component';
-import { ConfiguracionComponent } from './modulos/configuracion/configuracion.component';
-import { ContabilidadComponent } from './modulos/contabilidad/contabilidad.component';
-import { EquipoComponent } from './modulos/equipo/equipo.component';
-import { PlanillasComponent } from './modulos/planillas/planillas.component';
-import { RrhhComponent } from './modulos/rrhh/rrhh.component';
-import { TrabajadorComponent } from './modulos/trabajador/trabajador.component';
-import { InicioComponent } from './shared/inicio/inicio.component';
-import { HeaderComponent } from './shared/header/header.component';
+import { InicioModule } from './inicio/inicio.module';
+import { LoginModule } from './login/login.module';
+import { ModulosModule } from './modulos/modulos.module';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent,
-    AdministracionComponent,
-    ConfiguracionComponent,
-    ContabilidadComponent,
-    EquipoComponent,
-    PlanillasComponent,
-    RrhhComponent,
-    TrabajadorComponent,
-    InicioComponent,
-    HeaderComponent
+   
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    InicioModule,
+    LoginModule,
+    ModulosModule
   ],
   providers: [],
   bootstrap: [AppComponent]
